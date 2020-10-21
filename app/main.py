a5ae88e4bafbb9c44f5b5e584a67455d63d88592
Kanishkah Anwari <kanwari3@gatech.edu>
2020-10-20 21:14:36 -0400

    frontend-template

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__,template_folder='template', static_folder='static', static_url_path='')
+app.config.from_object(__name__)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, this is Team HZ-TASK!'
+def server_index():
+    return render_template("index.html")
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = 5000, debug = True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
