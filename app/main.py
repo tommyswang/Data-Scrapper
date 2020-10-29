cf964800aaf2e31c7361f1a1b5fd4c30546e5562
Allen Leigh <allenlsy@gmail.com>
2020-10-28 18:51:14 -0700

    patch db setup to pass the tests
    Hacky way to ignore db setup in testing environment

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from controllers.html import controller as html_controller
 from controllers.form import controller as form_controller
 
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__, template_folder='templates',
                 static_folder='static', static_url_path='')
     app.config.from_object(__name__)
@@ -41,23 +41,26 @@ def create_app():
         team_name = DemoParser.our_team_name()
         return render_template('hello.html', team_name=team_name)
 
+
+    '''
+        SQLAlchemy Setup
+        TODO: use OS variables or config file - hard code for now
+        This is our MySQL DB hosted on cloud, for now lets keep it like this
+        so project will build and we can keep working
+    '''
+    if not testing:
+        DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
+            user='root', password='abc123456', server='155.138.217.198', database='data_scrper')
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+        db = SQLAlchemy(app)
+
+        db.create_all()
+        db.session.commit()
+
     return app
 
 
-app = create_app()
-
-'''
-    SQLAlchemy Setup
-    TODO: use OS variables or config file - hard code for now
-    This is our MySQL DB hosted on cloud, for now lets keep it like this
-    so project will build and we can keep working
-'''
-DATABSE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
-    user='root', password='abc123456', server='155.138.217.198', database='data_scrper')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABSE_URI
-db = SQLAlchemy(app)
-
-db.create_all()
-db.session.commit()
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- app/tests/test_paths.py
+++ app/tests/test_paths.py
@@ -11,28 +11,28 @@ def get_page(path):
 
 
 def test_get_form():
-    app = create_app()
+    app = create_app(testing=True)
     with app.test_client() as c:
         resp = c.get("/form")
         assert resp.status_code == 200
 
 
 def test_get_api():
-    app = create_app()
+    app = create_app(testing=True)
     with app.test_client() as c:
         resp = c.get("/api")
         assert resp.status_code == 200
 
 
 def test_get_html():
-    app = create_app()
+    app = create_app(testing=True)
     with app.test_client() as c:
         resp = c.get("/html")
         assert resp.status_code == 200
 
 
 def test_get_pdf():
-    app = create_app()
+    app = create_app(testing=True)
     with app.test_client() as c:
         resp = c.get("/pdf")
         assert resp.status_code == 200
