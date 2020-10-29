ea4038d824246f031f68b7141d0636ae902f4155
Allen Leigh <allenlsy@gmail.com>
2020-10-28 18:30:21 -0700

    add controllers for each type of parsing process

---
+++ app/lib/controller/pdf.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+controller = Blueprint('pdf', __name__)
+
+
+@controller.route('/pdf', methods=['GET'])
+def pdf():
+    return render_template("pdf.html")
+
+
+@controller.route('/pdf', methods=['POST'])
+def create_pdf_job():
+    pass
--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-
-# format here is:
-# from <relative module file name> import <class name>
 from flask_cors import CORS
-from lib.parsers.demo_parser import DemoParser
+from lib.controller.api import controller as api_controller
+from lib.controller.pdf import controller as pdf_controller
+from lib.controller.html import controller as html_controller
+from lib.controller.form import controller as form_controller
 
 
 def create_app():
@@ -25,37 +25,10 @@ def create_app():
 
     CORS(app, resources={r'/*': {'origins': '*'}})
 
-    @app.route('/api', methods=['GET'])
-    def api():
-        return render_template("api.html")
-
-    @app.route('/api', methods=['POST'])
-    def create_api_job():
-        pass
-
-    @app.route('/form', methods=['GET'])
-    def form():
-        return render_template("form.html")
-
-    @app.route('/form', methods=['POST'])
-    def create_form_job():
-        pass
-
-    @app.route('/html', methods=['GET'])
-    def html():
-        return render_template("html.html")
-
-    @app.route('/html', methods=['POST'])
-    def create_html_job():
-        pass
-
-    @app.route('/pdf', methods=['GET'])
-    def pdf():
-        return render_template("pdf.html")
-
-    @app.route('/pdf', methods=['POST'])
-    def create_pdf_job():
-        pass
+    app.register_blueprint(api_controller)
+    app.register_blueprint(pdf_controller)
+    app.register_blueprint(html_controller)
+    app.register_blueprint(form_controller)
 
     @app.route('/job/<hash>')
     def get_job(hash):
@@ -71,21 +44,20 @@ def create_app():
     return app
 
 
-if __name__ == "__main__":
-    app = create_app()
+app = create_app()
 
-    '''
-        SQLAlchemy Setup
-        TODO: use OS variables or config file - hard code for now
-        This is our MySQL DB hosted on cloud, for now lets keep it like this
-        so project will build and we can keep working
-    '''
-    DATABSE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
-        user='root', password='abc123456', server='155.138.217.198', database='data_scrper')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABSE_URI
-    db = SQLAlchemy(app)
+'''
+    SQLAlchemy Setup
+    TODO: use OS variables or config file - hard code for now
+    This is our MySQL DB hosted on cloud, for now lets keep it like this
+    so project will build and we can keep working
+'''
+DATABSE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
+    user='root', password='abc123456', server='155.138.217.198', database='data_scrper')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABSE_URI
+db = SQLAlchemy(app)
 
-    db.create_all()
-    db.session.commit()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+db.create_all()
+db.session.commit()
+app.run(host='0.0.0.0', port=5000, debug=True)
