7933ff0deec2968bff0b32b73f8772e141e4a595
Kanishkah Anwari <kanwari3@gatech.edu>
2020-11-01 19:48:05 -0500

    changes from commeents and TODO list

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -1,10 +1,11 @@
 import csv
+from datetime import datetime
 
 from pathlib import Path
 
 import requests
 from flask import Blueprint, render_template, request, send_file, safe_join
-controller = Blueprint('api', __name__, )
+controller = Blueprint('api', __name__ )
 
 def getFhirResource(url):
     response = requests.get(url = url)
@@ -12,7 +13,7 @@ def getFhirResource(url):
         data = response.json()
         return data
     else:
-        return "error"
+        return None
 
 @controller.route('/api/download', methods=['POST'])
 def downloadFile():
@@ -36,14 +37,18 @@ def create_api_job():
         data = getFhirResource(URL)
     except:
         return render_template("api.html", error = True)
-    if data == "error":
+    if data is None:
         return render_template("api.html", error = True)
 
     print(data.keys())
     path = Path(controller.root_path).parent
-    uploadPath = str(Path.joinpath(path, 'static/client'))
+    uploadPath = str(Path.joinpath(path, 'static/tmp'))
     print(uploadPath)
-    data_file = open(uploadPath + '/data_file.csv', 'w')
+    now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    # TODO Use scrapeFile for file
+    data_file = open(uploadPath + f"/{now}data_file.csv", 'w')
+
+    # TODO Extract CSV creator
     csv_writer = csv.writer(data_file)
     header = data.keys()
     csv_writer.writerow(header)
@@ -52,6 +57,12 @@ def create_api_job():
     csv_writer.writerow(data.values())
 
     data_file.close()
-    print(data_file)
-    safe_path = safe_join(uploadPath, 'data_file.csv')
-    return render_template("api.html", error = False, safe_path = safe_path)
+    print("datafile:")
+
+    safe_path = safe_join(uploadPath, f"{now}data_file.csv")
+    if safe_path:
+        print(safe_path)
+    else:
+        return render_template("api.html")
+
+    return render_template("api.html", error = False, safe_path = safe_path, )
