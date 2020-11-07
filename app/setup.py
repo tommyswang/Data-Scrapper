6d4f94ccfd7b0cc4cf56e50815126eebdb73a078
Jung, Sege <sjung332@gatech.edu>
2020-11-07 13:02:32 -0500

    add prefix

--- app/setup.py
+++ app/setup.py
@@ -5,8 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 db = SQLAlchemy()
-app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='')
-app.config['APPLICATION_ROOT'] = '/data-scrapper-app'
 
 from models.scrape_job import ScrapeJob
 from models.scrape_file import ScrapeFile
@@ -26,6 +24,7 @@ env_name = os.environ.get('ENV', 'dev_local')  # default ENV is dev_local
 def create_app(testing=False):
     app = Flask(__name__, template_folder='templates',
                 static_folder='static', static_url_path='')
+    app.register_blueprint(controller, url_prefix='/data-scapper-app')
     app.config.from_object(__name__)
     app.logger.setLevel(logging.INFO)
     app.logger.info(f"Environment: {env_name}")
