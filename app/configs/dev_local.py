f92d3d6e0f6515981fab567a4f3ce76c07f1e0bd
Allen Leigh <allenlsy@gmail.com>
2020-11-08 09:50:13 -0800

    Use gunicorn as production web server, and nginx as reverse proxy
    This solves the issues of:
    
    * proper production server: using `python` command to start a server is
    not for production
    * Broken css on production
    * site-wide path generation problem, which previous `url_for` does not
    contain `/data-scrapper-app` subpath

--- app/configs/dev_local.py
+++ app/configs/dev_local.py
@@ -1,4 +1,5 @@
 from configs.common import *
 
+ENV = "dev_local"
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}@{server}/{database}'.format(
     user='root', server='127.0.0.1', database='data_scrapper')
