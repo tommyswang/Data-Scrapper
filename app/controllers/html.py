f61db9488d8dc59713fbc77110db2c26029f44cc
Zahiduzzaman Biswas <zbiswas3@gatech.edu>
2020-11-08 01:47:07 -0500

    add html request handler

--- app/controllers/html.py
+++ app/controllers/html.py
@@ -1,13 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from models.job_attr import JobType
+from models.scrape_job import ScrapeJob
 
 controller = Blueprint('html', __name__)
-
 
 @controller.route('/html', methods=['GET'])
 def html():
     return render_template("html.html")
 
-
 @controller.route('/html', methods=['POST'])
 def create_html_job():
-    pass
+    url = request.form['url']
+    job = ScrapeJob(JobType.HTML, url)
+    if job:
+        job.run()
+        return render_template("html.html", info="Running Job")
+    else:
+        return render_template("html.html", error="Error: No Job Found.")
