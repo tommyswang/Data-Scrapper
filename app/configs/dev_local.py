d084187d14e4fadf137aca2971d23ad9f9b996c0
Allen Leigh <allenlsy@gmail.com>
2020-11-09 19:51:18 -0800

    local db password reads from MYSQL_ROOT_PWD env var

--- app/configs/dev_local.py
+++ app/configs/dev_local.py
@@ -1,5 +1,13 @@
 from configs.common import *
+import os
 
 ENV = "dev_local"
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}@{server}/{database}'.format(
-    user='root', server='127.0.0.1', database='data_scrapper')
+
+db_pwd = os.environ.get('MYSQL_ROOT_PWD', None)
+
+if db_pwd:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
+        user='root', server='127.0.0.1', database='data_scrapper', password=db_pwd)
+else:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}@{server}/{database}'.format(
+        user='root', server='127.0.0.1', database='data_scrapper')
