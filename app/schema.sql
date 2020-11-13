17983a3e7c99d27d35cc58762ed199f959846c93
Allen Leigh <allenlsy@gmail.com>
2020-11-12 18:23:37 -0800

    add makefile to do db migration

---
+++ app/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS `scrape_file`;
+CREATE TABLE `scrape_file` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `path` text,
+  `created` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `scrape_job`;
+CREATE TABLE `scrape_job` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) DEFAULT NULL,
+  `jobType` varchar(20) DEFAULT NULL,
+  `jobInput` varchar(300) DEFAULT NULL,
+  `fileId` varchar(64) DEFAULT NULL,
+  `extra` text,
+  `name` varchar(64),
+  `created` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
