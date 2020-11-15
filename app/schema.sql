fa59a9ea7ff9a67330ffc0d9051d3dcfc74bb261
Allen Leigh <allenlsy@gmail.com>
2020-11-15 09:22:25 -0800

    improve schema.sql for mariadb

--- app/schema.sql
+++ app/schema.sql
@@ -1,14 +1,3 @@
-DROP TABLE IF EXISTS `scrape_file`;
-CREATE TABLE `scrape_file` (
-  `id` varchar(64) NOT NULL,
-  `name` varchar(64) DEFAULT NULL,
-  `path` text,
-  `sys_created_on` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `sys_updated_on` timestamp DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 
 DROP TABLE IF EXISTS `scrape_job`;
 CREATE TABLE `scrape_job` (
@@ -22,4 +11,17 @@ CREATE TABLE `scrape_job` (
   `sys_created_on` timestamp DEFAULT CURRENT_TIMESTAMP,
   `sys_updated_on` timestamp DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
+
+
+DROP TABLE IF EXISTS `scrape_file`;
+CREATE TABLE `scrape_file` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `path` text,
+  `sys_created_on` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `sys_updated_on` timestamp DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+);
+
