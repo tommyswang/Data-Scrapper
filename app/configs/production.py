43dfa2cc4ec6dbd54e7ce865bec8e947144a9548
Allen Leigh <allenlsy@gmail.com>
2020-11-15 09:06:50 -0800

    use remote database

--- app/configs/production.py
+++ app/configs/production.py
@@ -7,5 +7,4 @@ SCRIPT_NAME = "/data-scrapper-app"
 # remote db
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='abc123456', server='155.138.217.198', database='data_scrper')
 
-
-# SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', server='127.0.0.1', database='data_scrapper', password='abc123456')
+# SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', server='localhost', database='data_scrapper', password='abc123456')
