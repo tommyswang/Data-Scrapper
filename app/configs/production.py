2f89272e766eee77ed45453268f5dbf4441c2ed8
Cheng, Hongyu <hcheng98@gatech.edu>
2020-11-16 15:09:43 -0500

    Update production.py

--- app/configs/production.py
+++ app/configs/production.py
@@ -5,6 +5,6 @@ SERVER_NAME = "apps.hdap.gatech.edu"
 SCRIPT_NAME = "/data-scrapper-app"
 
 # remote db
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='abc123456', server='155.138.217.198', database='data_scrper')
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='abc123456', server='data-scrapper-db-service.ns-data-scrapper', database='data_scrper')
 
 # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', server='127.0.0.1', database='data_scrapper', password='abc123456')
